512bfcde42928b9899adc7a2d24ba9a3363c8a92
Mathieu SERVIERE <mathieu@math98.fr>
2023-02-05 13:51:49 +0100

    Accounts
     - Add Account entity, controller, repository and migration
     - Add AccountType form
     - Add views
     - Edit sidebar in front layout

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Account;
+use App\Form\AccountType;
+use App\Repository\AccountRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/account', name: 'app_account')]
+class AccountController extends AbstractController
+{
+    #[Route('/', name: '_index', methods: ['GET'])]
+    public function index(AccountRepository $accountRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        return $this->render('account/index.html.twig', [
+            'accounts' => $accountRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: '_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AccountRepository $accountRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $account = new Account();
+        $form = $this->createForm(AccountType::class, $account);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $accountRepository->save($account, true);
+
+            return $this->redirectToRoute('app_account_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('account/form.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Nouveau compte',
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: '_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Account $account, AccountRepository $accountRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $form = $this->createForm(AccountType::class, $account);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $accountRepository->save($account, true);
+
+            return $this->redirectToRoute('app_account_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('account/form.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Modifier le compte '.$account->getName(),
+            'button_label' => 'Modifier',
+        ]);
+    }
+
+    #[Route('/{id}', name: '_delete', methods: ['POST'])]
+    public function delete(Request $request, Account $account, AccountRepository $accountRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        if ($this->isCsrfTokenValid('delete'.$account->getId(), $request->request->get('_token'))) {
+            $accountRepository->remove($account, true);
+        }
+
+        return $this->redirectToRoute('app_account_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Account.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+class Account
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 8, scale: 2, nullable: true)]
+    private ?string $individual_price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIndividualPrice(): ?string
+    {
+        return $this->individual_price;
+    }
+
+    public function setIndividualPrice(?string $individual_price): self
+    {
+        $this->individual_price = $individual_price;
+
+        return $this;
+    }
+}
