115fdd913db3fdc6a13e89bbbd24dee5f1221854
Mathieu SERVIERE <mathieu@math98.fr>
2023-02-05 22:21:07 +0100

    Contracts
     - Add Contract entity, controller, repository and migration
     - Add ContractType form
     - Edit Account entity
     - Add views
     - Edit sidebar in front layout

---
+++ migrations/Version20230205155758.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230205155758 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE contract (id INT AUTO_INCREMENT NOT NULL, account_id INT NOT NULL, name VARCHAR(255) NOT NULL, start DATE NOT NULL, end DATE DEFAULT NULL, monthly_amount NUMERIC(8, 2) DEFAULT NULL, reception_delayed TINYINT(1) NOT NULL, billing_delayed TINYINT(1) NOT NULL, INDEX IDX_E98F28599B6B5FBA (account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE contract ADD CONSTRAINT FK_E98F28599B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contract DROP FOREIGN KEY FK_E98F28599B6B5FBA');
+        $this->addSql('DROP TABLE contract');
+    }
+}
