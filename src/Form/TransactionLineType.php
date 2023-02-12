457d1d980151a7494bc66bfbd69bd68fbfa93fef
Mathieu SERVIERE <mathieu@math98.fr>
2023-02-12 03:07:03 +0100

    Transactions
     - Add Transaction entity, controller and repository
     - Add TransactionLine entity and repository
     - Add migration
     - Add TransactionType form
     - Add TransactionLineType form
     - Edit Account entity
     - Add views
     - Edit sidebar in front layout

---
+++ src/Form/TransactionLineType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\TransactionLine;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TransactionLineType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('account', EntityType::class, [
+                'label' => false,
+                'class' => 'App\Entity\Account',
+                'choice_label' => 'name',
+                'placeholder' => 'Choisir un compte',
+                'row_attr' => [
+                    'class' => 'col-3',
+                ],
+            ])
+            ->add('amount', NumberType::class, [
+                'label' => false,
+                'row_attr' => [
+                    'class' => 'col-7',
+                ],
+            ])
+            ->add('remove', ButtonType::class, [
+                'label' => '<i class="fas fa-trash"></i>',
+                'label_html' => true,
+                'attr' => [
+                    'class' => 'btn btn-danger remove-line',
+                ],
+                'row_attr' => [
+                    'class' => 'd-grid col-2',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => TransactionLine::class,
+        ]);
+    }
+}
---
+++ src/Form/TransactionType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Transaction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TransactionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateTimeType::class, [
+                'label' => 'Date',
+                'widget' => 'single_text',
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+            ])
+            ->add('transactionLines', CollectionType::class, [
+                'label' => 'Lignes de transaction',
+                'entry_type' => TransactionLineType::class,
+                'entry_options' => [
+                    'label' => false,
+                    'attr' => ['class' => 'row'],
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Transaction::class,
+        ]);
+    }
+}
