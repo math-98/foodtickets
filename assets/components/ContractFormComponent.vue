f1b188c3ab0d21491abdac11bc91b2110cf33c55
Mathieu SERVIERE <mathieu@math98.fr>
2023-02-15 15:14:27 +0100

    VueJS components
     - Add VueJS components and load them in app.js
     - Edit views
     - Edit ContactController
     - Edit TransactionController
     - Setup serialization in the entities

---
+++ assets/components/ContractFormComponent.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="row mb-3">
+    <div class="col-6">
+      <label for="contract_name" class="form-label required">Nom</label>
+      <input
+          type="text"
+          id="contract_name"
+          class="form-control"
+          name="contract[name]"
+          v-model="contractData.name"
+          required
+      >
+    </div>
+    <div class="col-6">
+      <label class="form-label required" for="contract_account">Compte</label>
+      <select
+          id="contract_account"
+          class="form-select"
+          name="contract[account]"
+          v-model="contractData.accountId"
+          required
+      >
+        <option value="">Choisissez un compte</option>
+        <option :value="account.id" v-for="account of accounts">
+          {{ account.name }}
+        </option>
+      </select>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-6">
+      <label for="contract_start" class="form-label required">Début</label>
+      <input
+          type="date"
+          id="contract_start"
+          class="form-control"
+          name="contract[start]"
+          v-model="contractData.start"
+          required
+      >
+    </div>
+    <div class="col-6">
+      <label for="contract_end" class="form-label">Fin</label>
+      <input
+          type="date"
+          id="contract_end"
+          class="form-control"
+          name="contract[end]"
+          v-model="contractData.end"
+      >
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-4">
+      <label for="contract_monthly_amount" class="form-label">Montant mensuel</label>
+      <amount-input
+          name="contract[monthly_amount]"
+          :value="contractData.monthly_amount"
+          :price="(account) ? account.individual_price : -1"
+      ></amount-input>
+      <div id="contract_monthly_amount_help" class="form-text mb-0 help-text">
+        Laisser vide si le montant est variable (Forfait réel)
+      </div>
+    </div>
+    <div class="col-4 pt-3">
+      <div class="form-check">
+        <input
+            type="checkbox"
+            id="contract_reception_delayed"
+            class="form-check-input"
+            name="contract[reception_delayed]"
+            v-model="contractData.reception_delayed"
+            aria-describedby="contract_reception_delayed_help"
+        >
+        <label class="form-check-label" for="contract_reception_delayed">Réception décalée</label>
+      </div>
+      <div id="contract_reception_delayed_help" class="form-text mb-0 help-text">{{ receptionDelayedHelp }}</div>
+    </div>
+    <div class="col-4 pt-3">
+      <div class="form-check">
+        <input
+          type="checkbox"
+          id="contract_billing_delayed"
+          class="form-check-input"
+          name="contract[billing_delayed]"
+          v-model="contractData.billing_delayed"
+          aria-describedby="contract_billing_delayed_help"
+        >
+        <label class="form-check-label" for="contract_billing_delayed">Facturation décalée</label>
+      </div>
+      <div id="contract_billing_delayed_help" class="form-text mb-0 help-text">{{ billingDelayedHelp }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AmountInput from "./AmountInputComponent.vue";
+
+export default {
+  components: { AmountInput },
+  mounted() {
+    this.contractData = this.contract;
+    if (this.contractData.accountId === null) {
+      this.contractData.accountId = '';
+    }
+    if (this.contractData.start !== null) {
+      this.contractData.start = (new Date(this.contractData.start)).toISOString().substring(0, 10);
+    }
+    if (this.contractData.end !== null) {
+      this.contractData.end = (new Date(this.contractData.end)).toISOString().substring(0, 10);
+    }
+    if (this.contractData.monthly_amount !== null) {
+      this.contractData.monthly_amount = Number(this.contractData.monthly_amount);
+    }
+    this.contractData.billing_delayed = (this.contractData.billing_delayed == 1);
+    this.contractData.reception_delayed = (this.contractData.reception_delayed == 1);
+  },
+  data() {
+    return {
+      contractData: {},
+    }
+  },
+  computed: {
+    account() {
+      return this.accounts.find(account => account.id === this.contractData.accountId);
+    },
+    billingDelayedHelp() {
+      return 'Facturation à la fin du mois' +
+          ((this.contractData.billing_delayed) ? ' suivant' : '');
+    },
+    receptionDelayedHelp() {
+      return 'Réception au début du mois' +
+          ((this.contractData.reception_delayed) ? ' suivant' : '');
+    }
+  },
+  props: {
+    accounts: {
+      type: Array,
+      required: true,
+    },
+    contract: {
+      type: Object,
+      required: true,
+    },
+    errors: {
+      type: Object,
+      default: {},
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .form-check-label {
+    margin-bottom: 0;
+  }
+</style>
--- src/Controller/ContractController.php
+++ src/Controller/ContractController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Contract;
 use App\Form\ContractType;
+use App\Repository\AccountRepository;
 use App\Repository\ContractRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,41 +29,46 @@ class ContractController extends AbstractController
         Request $request,
         Contract $contract,
         ContractRepository $contractRepository,
+        AccountRepository $accountRepository,
         array $viewParams
     ): Response {
         $form = $this->createForm(ContractType::class, $contract);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($contract->getMonthlyAmount() && $contract->getAccount()->getIndividualPrice()) {
-                $contract->setMonthlyAmount(floor($contract->getAccount()->getIndividualPrice()));
-            }
-            $contractRepository->save($contract, true);
+        if ($form->isSubmitted()) {
+            $form->get('monthly_amount')->addError(new FormError('test'));
+            if ($form->isValid()) {
+                if ($contract->getMonthlyAmount() && $contract->getAccount()->getIndividualPrice()) {
+                    $contract->setMonthlyAmount(floor($contract->getAccount()->getIndividualPrice()));
+                }
+                $contractRepository->save($contract, true);
 
-            return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
+                return $this->redirectToRoute('app_contract_index', [], Response::HTTP_SEE_OTHER);
+            }
         }
 
         return $this->render('contract/form.html.twig', array_merge($viewParams, [
             'form' => $form->createView(),
+            'accounts' => $accountRepository->findAll(),
         ]));
     }
 
     #[Route('/new', name: '_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ContractRepository $contractRepository): Response
+    public function new(Request $request, ContractRepository $contractRepository, AccountRepository $accountRepository): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        return $this->handleForm($request, new Contract(), $contractRepository, [
+        return $this->handleForm($request, new Contract(), $contractRepository, $accountRepository, [
             'title' => 'Nouveau contrat',
         ]);
     }
 
     #[Route('/{id}/edit', name: '_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Contract $contract, ContractRepository $contractRepository): Response
+    public function edit(Request $request, Contract $contract, ContractRepository $contractRepository, AccountRepository $accountRepository): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        return $this->handleForm($request, $contract, $contractRepository, [
+        return $this->handleForm($request, $contract, $contractRepository, $accountRepository, [
             'button_label' => 'Modifier',
             'title' => 'Modifier le contrat '.$contract->getName(),
         ]);
